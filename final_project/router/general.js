f8d8e90be30f7b8e2f6b8fd16534fc23d6e595e1
“METWebServer” <webserver@themetropolitanschool.com>
2024-05-15 15:07:12 -0400

    updated until task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,26 +12,32 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.status(200).send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.status(200).send(JSON.stringify(req.params,null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const myArr = [];
+    for (let key in books) {
+        myArr.push(books[key]);
+    }
+    const filtered = myArr.filter(book => book.author === req.params.author);
+    res.status(200).send(JSON.stringify(filtered,null,4));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const myArr = [];
+    for (let key in books) {
+        myArr.push(books[key]);
+    }
+    const filtered = myArr.filter(book => book.title === req.params.title);
+    res.status(200).send(JSON.stringify(filtered,null,4));
 });
 
 //  Get book review
