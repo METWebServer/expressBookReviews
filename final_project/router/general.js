671891c397e6e9a0b25a1e8ad534656fe842435b
“METWebServer” <webserver@themetropolitanschool.com>
2024-05-15 17:14:34 -0400

    up till task 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,33 +20,49 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    res.status(200).send(JSON.stringify(books,null,4));
+public_users.get('/',async function (req, res) {
+    let myPromise = new Promise((resolve,reject) => {
+        resolve(books);
+    });
+    const solvedPromise = await myPromise;
+    res.status(200).send(JSON.stringify(solvedPromise,null,4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    res.status(200).send(JSON.stringify(req.params,null,4));
+public_users.get('/isbn/:isbn',async function (req, res) {
+    let myPromise = new Promise((resolve,reject) => {
+        resolve(req.params);
+    });
+    const solvedPromise = await myPromise;
+    res.status(200).send(JSON.stringify(solvedPromise,null,4));
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-    const myArr = [];
-    for (let key in books) {
-        myArr.push(books[key]);
-    }
-    const filtered = myArr.filter(book => book.author === req.params.author);
-    res.status(200).send(JSON.stringify(filtered,null,4));
+public_users.get('/author/:author',async function (req, res) {
+    let myPromise = new Promise((resolve,reject) => {
+        const myArr = [];
+        for (let key in books) {
+            myArr.push(books[key]);
+        }
+        const filtered = myArr.filter(book => book.author === req.params.author);
+        resolve(filtered);
+    });
+    const solvedPromise = await myPromise;
+    res.status(200).send(JSON.stringify(solvedPromise,null,4));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    const myArr = [];
-    for (let key in books) {
-        myArr.push(books[key]);
-    }
-    const filtered = myArr.filter(book => book.title === req.params.title);
-    res.status(200).send(JSON.stringify(filtered,null,4));
+public_users.get('/title/:title',async function (req, res) {
+    let myPromise = new Promise((resolve,reject) => {
+        const myArr = [];
+        for (let key in books) {
+            myArr.push(books[key]);
+        }
+        const filtered = myArr.filter(book => book.title === req.params.title);
+        resolve(filtered);
+    });
+    const solvedPromise = await myPromise;
+    res.status(200).send(JSON.stringify(solvedPromise,null,4));
 });
 
 //  Get book review
